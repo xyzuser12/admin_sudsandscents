711061bccb062fdfbe76082a45c69f0ea367acc2
Raymart <raymartformalejo00@gmail.com>
2023-05-16 20:08:21 +0800

    admin: padding top

--- pages/admins/new.js
+++ pages/admins/new.js
@@ -2,15 +2,35 @@ import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import axios from "axios";
 import Spinner from "@/components/Spinner";
+import { styled, Box } from "@mui/material";
 
 import Layout from "@/components/Layout";
 import AdminForm from "@/components/AdminForm";
 
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "space-between",
+  padding: theme.spacing(0, 1),
+  ...theme.mixins.toolbar,
+}));
+
 export default function NewAdminPage() {
   return (
     <Layout>
-      <h1>New Admin</h1>
-      <AdminForm />
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          p: 3,
+          overflow: "hidden",
+          margin: "3rem 1rem",
+        }}
+      >
+        <DrawerHeader />
+        <h1>New Admin</h1>
+        <AdminForm />
+      </Box>
     </Layout>
   );
 }
